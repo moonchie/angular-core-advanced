8d2e23e7a6663b2f00cb16dd900d1f57b652dd2c
shanshancao <shanshan.cao@thalesdigital.io>
2019-02-25 00:16:58 +0100

    add afterViewInit lifecycle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,35 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 import { COURSES } from '../db-data';
 import {Course} from './model/course';
+import {CourseCardComponent} from './course-card/course-card.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   courses = COURSES;
 
-  startDate = new Date(2019, 0, 21)
+  // Use ElementRef to elect native element in the template
+  @ViewChild('cardRef1', {read: ElementRef})
+  card1: ElementRef;
+
+  @ViewChild('container')
+  containerDiv: ElementRef;
+
+  constructor() {
+    // the viewChild is not populated if the view is not finished:
+    console.log('containerDiv', this.containerDiv);
+  }
+
+  // Implement the AfterViewInit lifecycle hook:
+  ngAfterViewInit() {
+    console.log('containerDiv', this.containerDiv);
+  }
 
   onCourseSelected(course: Course) {
-    console.log('You trigged the click event...', course);
+    // console.log('containerDiv', this.containerDiv);
+  console.log('card1', this.card1);
   }
 }
